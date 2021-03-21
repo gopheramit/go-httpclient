704ae2d38865349d22d8b0fc1a1de86fa0443553
gopheramit <amitagrawal10@outlook.com>
2021-03-21 16:33:14 +0530

    upto request body

--- example.go
+++ example.go
@@ -1,7 +1,60 @@
-package go_httpclient
+package main
 
 import (
+	"fmt"
+	"net/http"
+
 	"github.com/gopheramit/go-httpclient/gohttp"
 )
 
+var (
+	githubHttpClient = getGithubClient()
+)
 
+func getGithubClient() gohttp.HttpClient {
+
+	client := gohttp.New()
+
+	commonHeaders := make(http.Header)
+	commonHeaders.Set("Authorization", "Bearer ABC123")
+	client.SetHeaders(commonHeaders)
+
+	return client
+}
+
+func main() {
+	getUrls()
+}
+
+type User struct {
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+}
+
+func getUrls() {
+	//client := gohttp.New()
+
+	headers := make(http.Header)
+	//headers.Set("Authorization", "Bearer ABC123")
+
+	response, err := githubHttpClient.Get("https://api.github.com", headers)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(response.StatusCode)
+}
+
+func createUser(user User) {
+	//client := gohttp.New()
+
+	headers := make(http.Header)
+	//headers.Set("Authorization", "Bearer ABC123")
+
+	response, err := githubHttpClient.Post("https://api.github.com", headers, user)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(response.StatusCode)
+}
--- gohttp/client.go
+++ gohttp/client.go
@@ -1,21 +1,45 @@
 package gohttp
 
-type httpClient struct{}
+import "net/http"
 
-type HttpClient interface {
-	Get()
-	Put()
-	Patch()
-	Post()
-	Delete()
+type httpClient struct {
+	Headers http.Header
 }
 
-func (c *httpClient) Get() {}
+func New() HttpClient {
+	client := &httpClient{}
+	return client
+}
 
-func (c *httpClient) Put() {}
+type HttpClient interface {
+	SetHeaders(headers http.Header)
+	Get(url string, headers http.Header) (*http.Response, error)
+	Post(url string, headers http.Header, body interface{}) (*http.Response, error)
+	Put(url string, headers http.Header, body interface{}) (*http.Response, error)
+	Patch(url string, headers http.Header, body interface{}) (*http.Response, error)
+	Delete(url string, headers http.Header) (*http.Response, error)
+}
 
-func (c *httpClient) Post() {}
+func (c *httpClient) SetHeaders(headers http.Header) {
+	c.Headers = headers
+}
 
-func (c *httpClient) Patch() {}
+func (c *httpClient) Get(url string, headers http.Header) (*http.Response, error) {
+	return c.do(http.MethodGet, url, headers, nil)
 
-func (c *httpClient) Delete() {}
+}
+func (c *httpClient) Post(url string, headers http.Header, body interface{}) (*http.Response, error) {
+	return c.do(http.MethodPost, url, headers, body)
+}
+
+func (c *httpClient) Put(url string, headers http.Header, body interface{}) (*http.Response, error) {
+	return c.do(http.MethodPut, url, headers, body)
+}
+
+func (c *httpClient) Patch(url string, headers http.Header, body interface{}) (*http.Response, error) {
+	return c.do(http.MethodPatch, url, headers, body)
+}
+
+func (c *httpClient) Delete(url string, headers http.Header) (*http.Response, error) {
+	return c.do(http.MethodDelete, url, headers, nil)
+}
